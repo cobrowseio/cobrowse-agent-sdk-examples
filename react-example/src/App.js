43837dea85c189b43f1cdddca2febba1e0832587
Andy <andy@cobrowse.io>
2020-12-14 19:10:46 +0000

    add customisation example

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -12,12 +12,24 @@ function App(props) {
             <p>A list of devices whose details update in real time.</p>
             <DeviceList devices={props.devices} connect={props.connect} />
 
-            <p>Or a customized list of devices.</p>
-            { props.devices.map(d => <Device device={d}><button>Custom</button></Device>) }
-            
             <h2>Six Digit Code Entry</h2>
             <p>A component for accepting a screen share code.</p>
             <CodeEntry onCode={props.handleCode} />
+
+            <h2>Customization of Components</h2>
+            <p>Pick and choose which components are used.</p>
+            { props.devices.map(d => (
+                <div key={d.id} style={{border:`2px solid ${d.online?'orange':'#eee'}`, borderRadius:5, marginTop: 7}}>
+                    <Device style={{border: '0px none', marginTop: 7}} device={d}>
+                        <button>Custom Button</button>
+                    </Device>
+                    { Object.keys(d.custom_data).map(key => (
+                        <div style={{display:'inline-block', margin: 4, padding: '3px 7px', fontSize:12, borderRadius:10, background:'#f3f3f3'}} key={key}>
+                            <b>{key}</b><span> = </span><code>{d.custom_data[key]}</code>
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
     );
 }
