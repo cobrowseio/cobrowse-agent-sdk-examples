afa4167e6d4e1b155b528117b068e8a608805c49
Andy <andy@cobrowse.io>
2020-12-17 13:39:55 +0000

    start agent-sdk example

---
+++ agent-sdk-example/example.js
@@ -0,0 +1,6 @@
+import CobrowseAPI from 'cobrowse-agent-sdk';
+
+(async function() {
+    const api = new CobrowseAPI();
+    console.log(api);
+}());
---
+++ agent-sdk-example/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    resolve: {
+        modules: [
+            'node_modules',
+            path.resolve(__dirname, './')
+        ]
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './example.js',
+    module: {
+        rules: [
+            {
+                test: /^(?!.*\.spec\.js$).*\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+        ]
+    },
+    output: {
+        library: 'agent-sdk-example',
+        libraryTarget: 'umd',
+        filename: 'example.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    devServer: {
+        open: 'http://localhost:8123/example.html',
+        port: 8123,
+        publicPath: '/',
+    },
+};
