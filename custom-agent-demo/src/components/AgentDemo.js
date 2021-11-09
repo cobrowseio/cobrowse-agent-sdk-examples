fdd04a6441feda8eadf5803da033fb473132010f
Aaron Huggins <aaron@cobrowse.io>
2021-11-09 15:34:19 -0600

    Remove agent device polling and device ui

--- custom-agent-demo/src/components/AgentDemo.js
+++ custom-agent-demo/src/components/AgentDemo.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { Device, PlatformIcon, SmartConnectButton } from 'cobrowse-agent-ui'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faDoorOpen, faPen, faPencilAlt, faHandPointer } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faDoorOpen, faPen, faPencilAlt, faHandPointer, faGlobe } from '@fortawesome/free-solid-svg-icons'
 import CobrowseAPI from 'cobrowse-agent-sdk'
 import Button from './Button'
 import './AgentDemo.css'
@@ -9,23 +8,13 @@ import './AgentDemo.css'
 const cobrowse = new CobrowseAPI()
 let context = null
 
-function arrayEquals(a, b) {
-  if (Array.isArray(a) && Array.isArray(b)) {
-    a = a.sort()
-    b = b.sort()
-  } else {
-    return false
-  }
-
-  return a.length === b.length && a.every((val, index) => val === b[index])
-}
-
 export default class AgentDemo extends Component {
   constructor () {
     super()
     const query = new URLSearchParams(window.location.search)
     this.state = {
       demo_id: query.get('demo_id'),
+      connect: false,
       device_id: null,
       devices: [],
       token: query.get('token')
@@ -35,37 +24,14 @@ export default class AgentDemo extends Component {
     cobrowse.token = this.state.token
   }
 
-  renderDevices () {
-    const { devices } = this.state
-
-    if (devices.length > 0) {
-      return devices.map(device => {
-        return (
-          <div key={device.id} className='dvc-container' style={{ borderColor: device.online ? '#3cc56e' : '#eee' }}>
-            <Device device={device}>
-            <SmartConnectButton
-              key={device.id}
-              label={
-                <div className='smartbtn-content'>
-                  <PlatformIcon platform={device.device.platform} />
-                  <span> Join</span>
-                </div>
-              }
-              onClick={() => this.connect(device.id)}
-              device={device}
-            />
-            </Device>
-            {Object.keys(device.custom_data).map(key => (
-              <div className='custom-data' key={key}>
-                <b>{key}</b><span> = </span><code>{device.custom_data[key]}</code>
-              </div>
-            ))}
-          </div>
-        )
-      })
-    }
-
-    return null
+  renderConnect () {
+    return (
+      <div className='btn-row btn-connect row center-xs'>
+        <Button onClick={() => this.connect()}>
+          <FontAwesomeIcon icon={faGlobe} /><span> Connect</span>
+        </Button>
+      </div>
+    )
   }
 
   renderSession () {
@@ -78,7 +44,7 @@ export default class AgentDemo extends Component {
           frameBorder={0}
           width='100%'
           height={520}
-          src={`https://cobrowse.io/connect/device/${this.state.device_id}?token=${this.state.token}&end_action=none&agent_tools=none&device_controls=none&nochrome=true`}
+          src={`https://cobrowse.io/connect?filter_cobrowseio_demo_id=${this.state.demo_id}&token=${this.state.token}&end_action=none&agent_tools=none&device_controls=none&nochrome=true`}
         />
         <div className='btn-row row center-xs'>
           <Button onClick={() => context.setTool('laser')} className='column btn-left-most'>
@@ -105,65 +71,28 @@ export default class AgentDemo extends Component {
     return (
       <div className='AgentDemo'>
         {
-          this.state.device_id
+          this.state.connect
             ? this.renderSession()
-            : this.renderDevices()
+            : this.renderConnect()
         }
       </div>
     )
   }
 
   connect (deviceId) {
-    this.setState({ device_id: deviceId })
+    this.setState({ connect: true })
   }
 
   disconnect () {
-    this.setState({ device_id: null })
+    this.setState({ connect: false })
     if (context) {
       context.destroy()
       context = null
     }
   }
 
-  async getDevices () {
-    const devices = await cobrowse.devices.list()
-    const deviceIds = devices.map(device => device.id)
-    const { devices: old } = this.state
-    const oldIds = old.map(device => device.id)
-
-    // If the device sets are the same, do nothing with the data.
-    if (arrayEquals(deviceIds, oldIds)) return
-
-    if (Array.isArray(devices)) {
-      devices.forEach(device => {
-        device.subscribe()
-        device.on('updated', () => {
-          this.setState({ state: this.state })
-        })
-      })
-    }
-
-    if (Array.isArray(old)) {
-      old.forEach(device => {
-        device.unsubscribe()
-      })
-    }
-
-    this.setState({ devices })
-  }
-
-  refreshDevices () {
-    if (!this.state.device_id && context === null) {
-      this.getDevices()
-    }
-
-    setTimeout(() => {
-      this.refreshDevices()
-    }, 10000)
-  }
-
   componentDidUpdate () {
-    if (this.state.device_id && context === null) {
+    if (this.state.connect && context === null) {
       cobrowse.attachContext(this.sessionFrame.current).then(result => {
         context = result
 
@@ -172,9 +101,5 @@ export default class AgentDemo extends Component {
         })
       })
     }
-  }
-
-  componentDidMount () {
-    this.refreshDevices()
   }
 }
