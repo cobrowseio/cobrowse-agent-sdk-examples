25a44e86bf3349a6ccc030d41eb72554c29b1347
Andy Jones <andyjones@users.noreply.github.com>
2025-06-27 16:58:18 +0100

    Update custom agent demo to use vite (#16)

---
+++ custom-agent-demo/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '',
+  build: {
+    target: browserslistToEsbuild(),
+    outDir: 'build',
+  },
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+})
